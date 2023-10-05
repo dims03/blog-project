29e2adab1d6ff223df9d02d60b89ffcc2d462529
DenDims <dimasp95@icloud.com>
2023-10-05 14:34:52 +0800

    update

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,48 +1,24 @@
 <header class="flex items-center justify-between py-3 px-6 border-b border-gray-100">
     <div id="header-left" class="flex items-center">
         <a href="{{ route('home') }}">
-            <div class="text-gray-800 font-semibold">
-                <span class="text-blue-500 text-xl">&lt;Dims&gt;</span>
-            </div>
+            <x-application-mark class="block h-9 w-auto" />
         </a>
         <div class="top-menu ml-10">
             <ul class="flex space-x-4">
                 <li>
-                    <a class="flex space-x-2 items-center hover:text-blue-900 text-sm text-blue-500"
+                    {{-- <a class="flex space-x-2 items-center hover:text-blue-900 text-sm text-blue-500"
                         href="http://127.0.0.1:8000">
                         Home
-                    </a>
+                    </a> --}}
+                    <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
+                        {{ __('Home') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Blog') }}
+                    </x-nav-link>
                 </li>
 
-                <li>
-                    <a class="flex space-x-2 items-center hover:text-blue-500 text-sm text-gray-500"
-                        href="http://127.0.0.1:8000/blog">
-                        Blog
-                    </a>
-                </li>
-
-                <li>
-                    <a class="flex space-x-2 items-center hover:text-blue-500 text-sm text-gray-500"
-                        href="http://127.0.0.1:8000/blog">
-                        About Us
-                    </a>
-                </li>
-
-                <li>
-                    <a class="flex space-x-2 items-center hover:text-blue-500 text-sm text-gray-500"
-                        href="http://127.0.0.1:8000/blog">
-                        Contact Us
-                    </a>
-                </li>
-
-                <li>
-                    <a class="flex space-x-2 items-center hover:text-blue-500 text-sm text-gray-500"
-                        href="http://127.0.0.1:8000/blog">
-                        Terms
-                    </a>
-                </li>
-
-            </ul>
+               
         </div>
     </div>
     <div id="header-right" class="flex items-center md:space-x-6">
